4732ab42eea2aae46a689f3b64003758a37b4d4b
ishwar181818 <ishwarharbade55@gmail.com>
2025-03-15 15:02:54 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{ useEffect }  from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
@@ -9,11 +9,18 @@ import CourseDetails from "./components/CourseDetails";
 import TeachingMode from "./components/TeachingMode";
 import DemoLecture from "./components/DemoLecture";
 import JobOpening from "./components/JobOpening";
+import AnalyticsTracker from "./components/AnalyticsTracker";
+import ReactGA from "react-ga4";
 
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize("G-MCM3LJBCGW"); // Replace with your Measurement ID
+  }, []);
   return (
     <Router>
+       <AnalyticsTracker /> {/* Tracks route changes */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
