932ec531d502111f5fc110c1f6a2f4245cf04928
ishwar181818 <ishwarharbade55@gmail.com>
2026-05-10 16:15:21 +0530

    add

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       {/* Background Video */}
       <div className="video-background">
         <video autoPlay loop muted playsInline>
-          <source src="/background-video.mp4" type="video/mp4" />
+          <source src="/back.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
